022f6cbfcdfb0033ce850c9aec1787515c9207c4
tintin <tin871001@gmail.com>
2023-09-21 17:00:55 +0800

    add list

--- index.php
+++ index.php
@@ -7,8 +7,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
-<body>
-    <h1>Course</h1>
-    <button class="btn btn-primary">Test</button>
+<body class="container-fluid">
+    <div class="row">
+        <h1>張雅芬老師 112上學期課堂影片</h1>
+    </div>
+    <div class="row">
+        <div class="list-group">
+            <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
+                The current button
+            </button>
+            <button type="button" class="list-group-item list-group-item-action">A second item</button>
+            <button type="button" class="list-group-item list-group-item-action">A third button item</button>
+            <button type="button" class="list-group-item list-group-item-action">A fourth button item</button>
+            <button type="button" class="list-group-item list-group-item-action" disabled>A disabled button item</button>
+        </div>
+    </div>
+    
+    
 </body>
 </html>
